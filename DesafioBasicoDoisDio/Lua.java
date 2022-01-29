0a9e22def08bd05331fdadb8bab126a098c1f738
Paty <78480425+PatriciaCS1995@users.noreply.github.com>
2022-01-28 21:43:39 -0300

    UpDate2
    correção de linhas e comentários do desafio.

--- DesafioBasicoDoisDio/Lua.java
+++ DesafioBasicoDoisDio/Lua.java
@@ -2,8 +2,15 @@ package DesafioBasicoDoisDio;
 
 import java.util.Scanner;
 
-/*Jade ganhou de presente de aniversário um telescópio e ficou muito feliz, pois adora olhar a lua à noite. Ela sempre foi uma estudante muito boa, e só analizando a lua por duas noites seguidas, já consegue identificar as mudanças que ocorreram na iluminação e o percentual aproximado da lua que está iluminada.
-Você, que é amigo da Jade e estuda Computação, resolveu fazer um pequeno programa que, baseado nesta avaliação que ela fez nas duas últimas noites, informa a fase na qual a lua se encontra. Se a porção visível da lua no momento estiver entre 0 e 2%, por exemplo, é lua nova, se for entre 3 e 96% é lua crescente, se for entre 97 e 100% é lua cheia e se for entre 96 e 3% (diminuindo) é lua minguante.
+/*Jade ganhou de presente de aniversÃ¡rio um telescÃ³pio e ficou muito feliz, pois adora olhar a lua Ã  noite.
+Ela sempre foi uma estudante muito boa, e sÃ³ analizando a lua por duas noites seguidas, 
+jÃ¡ consegue identificar as mudanÃ§as que ocorreram na iluminaÃ§Ã£o e o percentual aproximado da lua que estÃ¡ iluminada.
+VocÃª, que Ã© amigo da Jade e estuda ComputaÃ§Ã£o, resolveu fazer um pequeno programa que, 
+baseado nesta avaliaÃ§Ã£o que ela fez nas duas Ãºltimas noites, informa a fase na qual a lua se encontra.
+Se a porÃ§Ã£o visÃ­vel da lua no momento estiver entre 0 e 2%, por exemplo, Ã© lua nova, 
+se for entre 3 e 96% Ã© lua crescente, 
+se for entre 97 e 100% Ã© lua cheia 
+e se for entre 96 e 3% (diminuindo) Ã© lua minguante.
 */
 
 
@@ -11,23 +18,27 @@ public class Lua {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		  int inicio = sc.nextInt();
-		  int fim = sc.nextInt();
+		 
+		int inicio = sc.nextInt();
+		int fim = sc.nextInt();
 		    
-		        if (fim >= 97 && fim <= 100) {
+		        if (fim >= 97 && fim <= 100)
+			{
 		            System.out.println("cheia");
 		        }
-		        else if (fim >= 0 && fim <= 2) {
+		        else if (fim >= 0 && fim <= 2) 
+			{
 		            System.out.println("nova");
-		            }
-		        else if (fim >= 3 && fim <= 96 && fim > inicio) {
+		        }
+		        else if (fim >= 3 && fim <= 96 && fim > inicio) 
+			{
 		            System.out.println("crescente");
-		            }
-		        else {
+		        }
+		        else 
+			{
 		            System.out.println("minguante");
 		        }
-		    
-	sc.close();
+		
+		sc.close();
+		}
 	}
-
-}
